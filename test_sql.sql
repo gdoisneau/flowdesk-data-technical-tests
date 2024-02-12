b4d4af3e9fb76973021302d53b3ba340f9cf20f3
PiNaCl <gabriel.doisneau@laposte.net>
2024-02-12 21:45:09 +0100

    SQL, hope LATERAL JOINS are good

--- test_sql.sql
+++ test_sql.sql
@@ -1,11 +1,23 @@
 with trades as (
     select *
     from trades
+    ORDER BY executed_at 
 ),
 indexes as (
     select *
     from indexes
+    ORDER BY updated_at
 ),
-result as (...),
+result as (
+    select t.transaction_id, ind.updated_at, ind.exchange, ind.exchange_type from trades t
+    LEFT JOIN LATERAL (
+        SELECT * from indexes i
+        ORDER BY ABS(EXTRACT(EPOCH FROM t.executed_at - i.updated_at))
+        LIMIT 1
+    ) ind ON  t.currency_1 = ind.currency_1 
+    AND  t.currency_2 = ind.currency_2
+    AND t.exchange_type = ind.exchange_type
+    AND t.exchange = ind.exchange 
+)
 select *
-From result
+From result;
